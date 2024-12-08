4e7a7e7466d0db91f74f0c0d87abbbbdc66235da
Kenny Goodin <kennethbgoodin@gmail.com>
2024-12-08 01:27:08 -0500

    add 2024 day 8

---
+++ 2024/day8/main_test.go
@@ -0,0 +1,88 @@
+package day8
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/kungfukennyg/adventofgode/common"
+)
+
+const testInput = `............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............`
+
+func Test_findAntinodes(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{
+			args: args{testInput},
+			want: 14,
+		},
+		{
+			args: args{common.Input()},
+			want: 0,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := findAntinodes(tt.args.input); got != tt.want {
+				t.Errorf("findAntinodes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+const part2Input = `T.........
+...T......
+.T........
+..........
+..........
+..........
+..........
+..........
+..........
+..........`
+
+func Test_findAntinodesAnyDistance(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{
+			args: args{testInput},
+			want: 34,
+		},
+		{
+			args: args{part2Input},
+			want: 9,
+		},
+		{
+			args: args{common.Input()},
+			want: 809,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := findAntinodesAnyDistance(tt.args.input); got != tt.want {
+				t.Errorf("findAntinodesAnyDistance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
