1a4cd550065fc51018a37069ed47e0388641283b
Kenny Goodin <kennethbgoodin@gmail.com>
2024-12-07 21:43:34 -0500

    add 2015 day 2

---
+++ 2015/day2/main_test.go
@@ -0,0 +1,68 @@
+package day2
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/kungfukennyg/adventofgode/common"
+)
+
+func Test_wrappingPaper(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{
+			args: args{"2x3x4"},
+			want: 58,
+		},
+		{
+			args: args{"1x1x10"},
+			want: 43,
+		},
+		{
+			args: args{common.Input()},
+			want: 1588178,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := wrappingPaper(tt.args.input); got != tt.want {
+				t.Errorf("wrappingPaper() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_ribbonLength(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{
+			args: args{"2x3x4"},
+			want: 34,
+		},
+		{
+			args: args{"1x1x10"},
+			want: 14,
+		},
+		{
+			args: args{common.Input()},
+			want: 3783758,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := ribbonLength(tt.args.input); got != tt.want {
+				t.Errorf("ribbonLength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
