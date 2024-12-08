cce4385f8de1c46e10e26ae197673c52484a5218
Kenny Goodin <kennethbgoodin@gmail.com>
2024-12-07 22:03:44 -0500

    add 2024 day 4

---
+++ 2024/day4/main_test.go
@@ -0,0 +1,70 @@
+package day4
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/kungfukennyg/adventofgode/common"
+)
+
+func Test_countPattern(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{
+			args: args{
+				input: "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX",
+			},
+			want: 18,
+		},
+		{
+			args: args{
+				input: common.Input(),
+			},
+			want: 2336,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			g := new(tt.args.input)
+			if got := g.countPattern("XMAS"); got != tt.want {
+				t.Errorf("countPattern() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_crossPattern(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{
+			args: args{
+				input: "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX",
+			},
+			want: 9,
+		},
+		{
+			args: args{
+				input: common.Input(),
+			},
+			want: 1831,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			g := new(tt.args.input)
+			if got := g.crossPattern("MAS", 'A'); got != tt.want {
+				t.Errorf("crossPattern() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
