4f57ff6779b24a9238d0ce6509045a4bd80959ad
Kenny Goodin <kennethbgoodin@gmail.com>
2024-12-09 03:27:30 -0500

    2015/day14: add

---
+++ 2015/day14/main_test.go
@@ -0,0 +1,68 @@
+package day14
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/kungfukennyg/adventofgode/common"
+)
+
+func Test_furthestDistance(t *testing.T) {
+	type args struct {
+		input   string
+		seconds int
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{
+			args: args{
+				input:   "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\nDancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+				seconds: 1000,
+			},
+			want: 1120,
+		},
+		{
+			args: args{input: common.Input(), seconds: 2503},
+			want: 2660,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := furthestDistance(tt.args.input, tt.args.seconds); got != tt.want {
+				t.Errorf("furthestDistance) = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_mostPoints(t *testing.T) {
+	type args struct {
+		input   string
+		seconds int
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{
+			args: args{
+				input:   "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\nDancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+				seconds: 1000,
+			},
+			want: 689,
+		},
+		{
+			args: args{input: common.Input(), seconds: 2503},
+			want: 1256,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := mostPoints(tt.args.input, tt.args.seconds); got != tt.want {
+				t.Errorf("mostPoints() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
