c63846909971f171b9b3332a8cea65dd9a32a3cd
Kenny Goodin <kennethbgoodin@gmail.com>
2024-12-10 21:24:47 -0500

    2015/day16: add

---
+++ 2015/day16/main.go
@@ -0,0 +1,67 @@
+package day16
+
+import (
+	"slices"
+	"strings"
+
+	"github.com/kungfukennyg/adventofgode/common"
+)
+
+func whichSue(input, compounds string, greater, lesser []string) int {
+	people := parsePeople(input)
+	comps := parseCompounds(compounds)
+
+outer:
+	for i, p := range people {
+		for comp, want := range comps {
+			got, ok := p[comp]
+			if !ok {
+				continue
+			}
+			if ok := slices.Contains(greater, comp); ok {
+				if got <= want {
+					continue outer
+				}
+			} else if ok := slices.Contains(lesser, comp); ok {
+				if got >= want {
+					continue outer
+				}
+			} else if want != got {
+				continue outer
+			}
+		}
+
+		return i + 1
+	}
+	return -1
+}
+
+func parsePeople(input string) []map[string]int {
+	people := []map[string]int{}
+	for _, line := range common.Lines(input) {
+		person := parsePerson(line)
+		people = append(people, person)
+	}
+	return people
+}
+
+func parsePerson(line string) map[string]int {
+	out := map[string]int{}
+	skip := strings.Index(line, ": ") + 1
+	line = line[skip:]
+	for _, p := range strings.Split(line, ",") {
+		parts := strings.Split(p, ":")
+		prop := strings.TrimSpace(parts[0])
+		out[prop] = common.MustAtoi(strings.TrimSpace(parts[1]))
+	}
+	return out
+}
+
+func parseCompounds(compounds string) map[string]int {
+	o := map[string]int{}
+	for _, line := range common.Lines(compounds) {
+		parts := strings.Split(line, ":")
+		o[parts[0]] = common.MustAtoi(strings.TrimSpace(parts[1]))
+	}
+	return o
+}
